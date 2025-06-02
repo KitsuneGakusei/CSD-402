61d50f4026b61256df3e4bddfbd8e0eda66cc02f
Crystal Long <crylong@my365.bellevue.edu>
2025-06-02 18:33:20 -0400

    Add completed Module 1 Java program files and structure

--- module-1/LongMod1Program/src/LongMod1Program.java
+++ module-1/LongMod1Program/src/LongMod1Program.java
@@ -1,3 +1,11 @@
+/*
+ * Name: Crystal Long
+ * Date: June 2, 2025
+ * Assignment: Module 1.1 – Basic Java Program
+ */
+// Description: This program calculates the energy required to heat a given mass of water
+// from an initial temperature to a final temperature using the formula:
+ // Energy (Joules) = mass (kg) * specific heat capacity of water (4184 J/kg°C) * change in temperature (°C)
 import java.util.Scanner;
 
 public class LongMod1Program {
